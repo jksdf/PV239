e148fb712b993c2d4c4c0dfe527b44ae5c7ffc0e
Lukáš Kurčík <lukas.kurcik@gmail.com>
2019-04-08 22:14:33 +0200

    Calendar activity

---
+++ app/src/main/java/ly/betime/shuriken/activities/CalendarActivity.java
@@ -0,0 +1,14 @@
+package ly.betime.shuriken.activities;
+
+import android.os.Bundle;
+
+import ly.betime.shuriken.R;
+
+public class CalendarActivity extends AMenuActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+}
